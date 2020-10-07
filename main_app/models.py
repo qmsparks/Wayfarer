99560bac4af7a3ff36c649a3c3ceb51977b58381
cessig <cessig4161994@gmail.com>
2020-10-07 19:21:05 -0400

    Added the models needs work on order as well as installed a new dependency called pillows to allow for image upload with django

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+
+class City(models.Model):
+    city_name = models.CharField(max_length=50)
+    country_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    content = models.TextField(max_length=250)
+    date = models.DateField('created date')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    city = models.ManyToManyField(City)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=20)
+    current_city = models.CharField(max_length=50)
+    date = models.DateField('joined on')
+    profile_Main_Img = models.ImageField(upload_to='images/') 
+
+    def __str__(self):
+        return self.name
+
+
+class Meta:
+    ordering = ['-date']
