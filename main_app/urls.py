ebb23848f66b344d86d1febda6e08f4f48e33e82
donavonelli <donavonelli@yahoo.com>
2020-10-08 10:06:34 -0400

    Adds urls and views

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('profile/', views.profile_page, name='profile_page'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail')
 ]
