98d653f7c926e0570bffa228804998cbd907aaa9
rodmarrec <romarron01@gmail.com>
2020-10-13 14:02:18 -0700

    sets debug to true

--- wayfarer/settings.py
+++ wayfarer/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'obv$q^%%6x@ifd999ilqhkdm3_$&_lrtat@b%hk&==@lkl!i%s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
