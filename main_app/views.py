231f9a3d98d1ac2c69b1ed691285b5149c493625
cessig <cessig4161994@gmail.com>
2020-10-13 12:59:14 -0400

    working code on corey branch

--- main_app/views.py
+++ main_app/views.py
@@ -106,7 +106,7 @@ def post_detail(request, post_id):
 @login_required
 def post_edit(request, post_id):
     post = Post.objects.get(id=post_id)
-    if request.user.id == profile.user_id:
+    if request.user.id == post.profile.user_id:
         if request.method == 'POST':
             post_form = Post_Form(request.POST, instance=post)
             if post_form.is_valid():
